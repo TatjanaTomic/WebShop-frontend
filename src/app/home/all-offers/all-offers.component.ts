2cb7c8b8cfbfc32765a513446872d00f0d0ab160
TatjanaTomic <tatjanatomic997@yahoo.com>
2023-02-05 12:28:50 +0100

    Implement pagination

--- src/app/home/all-offers/all-offers.component.ts
+++ src/app/home/all-offers/all-offers.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
 import { Offer } from 'src/app/models/Offer';
 import { OffersService } from 'src/app/services/offers-service/offers.service';
 
@@ -13,16 +14,16 @@ import { OffersService } from 'src/app/services/offers-service/offers.service';
 export class AllOffersComponent implements OnInit {
   
   public array: Offer[] = [];
-  public displayedColumns = ['', '', '', '', ''];
   public dataSource: any;  
 
-  public pageSize = 10;
+  public pageSize = 8;
   public currentPage = 0;
   public totalSize = 0;
   
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  obs: Observable<any> | undefined;
 
-  constructor(private offersService: OffersService, private http: HttpClient) {}
+  constructor(private offersService: OffersService, private http: HttpClient, private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.getArray();
@@ -37,8 +38,10 @@ export class AllOffersComponent implements OnInit {
   private getArray() {
     this.offersService.findAll().subscribe(
       (response) => {
+        this.changeDetectorRef.detectChanges();
         this.dataSource = new MatTableDataSource<Offer>(response);
         this.dataSource.paginator = this.paginator;
+        this.obs = this.dataSource.connect();
         this.array = response;
         this.totalSize = this.array.length;
         this.iterator();
@@ -51,4 +54,5 @@ export class AllOffersComponent implements OnInit {
     const part = this.array.slice(start, end);
     this.dataSource = part;
   }
+
 }
